18e008b06ca7ec0de1bc977a0cf43adffd09088f
Adam Jurkiewicz <a.jurkiewicz5@gmail.com>
2023-04-21 15:50:25 +0200

    DEV-68 Add working MVP of krew-info-scraper
    - Returns valid JSON that matches the krew.info/zapasy data.
    - Todo: Finalize the format so it's matching the requirements.

--- scraper.py
+++ scraper.py
@@ -32,12 +32,14 @@ def fetch_all_cities_blood_types(row):
     pass
 
 
+# ['0 Rh-', '0 Rh+', 'A Rh-', 'A Rh+', 'B Rh-', 'B Rh+', 'AB Rh-', 'AB Rh+']
 def get_all_blood_types(rows):
     blood_types = []
     for row in rows:
         blood_types.append(row.find('h3').text)
     return blood_types
 
+# ['Białystok', 'Bydgoszcz', 'Gdańsk', 'Kalisz', 'Katowice', 'Kielce', 'Kraków', 'Lublin', 'Łódź', 'Olsztyn', 'Opole', 'Poznań', 'Racibórz', 'Radom', 'Rzeszów', 'Słupsk', 'Szczecin', 'Wałbrzych', 'Warszawa', 'Wrocław', 'Zielona Góra']
 def get_all_cities(rows):
     cities = []
     for row in rows:
@@ -65,17 +67,25 @@ for row in rows:
     bank_status.append(get_bank_status(row))
 
 blood_types_cities = {}
+# bloodtype_i = 0
+# city_i = 0
 
-for city in cities:
-    i = 0
+# print(cities)
+# print(blood_types)
+
+for count_cities, city in enumerate(cities):
+    # if city_i > 20 or bloodtype_i > 7:
+    #     break
     blood_types_cities[city] = {}
-    for blood_type in blood_types:
-        blood_types_cities[city][blood_type] = bank_status[i]
-        i += 1
+    for count_bd, blood_type in enumerate(blood_types):
+        blood_types_cities[city][blood_type] = bank_status[count_bd][count_cities]
+    #     bloodtype_i += 1
+    # city_i += 1
 
 import json
-
+# print(blood_types_cities)
 print(json.dumps(blood_types_cities, indent=2))
 
+# TODO: main function for clarity
 
 
